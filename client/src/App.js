b5d0f7970e0c92d5b106d4d535c1836629f16f29
Sahil Sagvekar <sahilsagvekar230@gmail.com>
2024-03-27 15:06:15 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,20 @@ import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
+import axios from "axios";
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   const isAuth = Boolean(useSelector((state) => state.token));
+
+  axios.defaults.withCredentials = true;
+  const handleSubmit = (e) => {
+  e.preventDefault();
+    axios.post("full-stak-social-media-app-1j6t.vercel.app/register",{name, email,password})
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
 
   return (
     <div className="app">
